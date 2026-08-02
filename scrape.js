9b2dd71f5f49a33252f551591f696e2578238ca6
matriculer <83557510+matriculer@users.noreply.github.com>
2026-08-02 16:54:26 -0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -5,7 +5,7 @@ const context = await browser.newContext({ acceptDownloads: true });
 const page = await context.newPage();
 await page.goto('https://www.puc-rio.br/microhorario');
 await page.viewportSize({width: 1080, height: 1024});
-await page.getByRole('button', { name: 'Ok' }).click()
+//await page.getByRole('button', { name: 'Ok' }).click()
 await page.locator('#btnBuscar').click()
 await page.locator('#pnlBaixarInfo').click()
 await page.locator('#ddlExtensao').selectOption("Texto");
